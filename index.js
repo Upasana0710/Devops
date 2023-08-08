411c3125af644f24eb37b6f8b3ec89841890d288
Upasana <upasanachaudhuri0710@gmail.com>
2023-08-08 14:05:42 +0530

    body parser added

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ import notesRoutes from './src/routes/notes.js';
 
 dotenv.config();
 const app = express();
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 
 app.use('/notes', notesRoutes);
 
 const PORT = 5000;
 mongoose.connect(process.env.MONGO_URL)
-    .then(()=>app.listen(PORT, ()=> {
+    .then(() => app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     }))
-    .catch((err)=>console.log(err))
+    .catch((err) => console.log(err))
