0a90e4453206e1059f8b94a7b980398b0d9dbd77
Upasana <upasanachaudhuri0710@gmail.com>
2023-08-23 15:01:33 +0530

    change routes

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post('/create', createNotes);
 router.get('/get/:id', getNote);
-router.get('/get', getNotes);
+router.get('/getNotes', getNotes);
 router.patch('/update/:id', updateNotes);
 router.delete('/delete/:id', deleteNotes);
 router.get('/ping', ping);
